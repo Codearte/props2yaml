78467e716470aea47a014ede5fe246578f951e81
Mariusz Smykuła <mariuszs@localhost.localdomain>
2015-06-02 10:36:42 +0200

    key extractor refactor

---
+++ src/main/java/io/codearte/p2yaml/KeyTreeBuilder.java
@@ -0,0 +1,65 @@
+package io.codearte.p2yaml;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class KeyTreeBuilder {
+
+    private final String key;
+    private final Object value;
+
+    public KeyTreeBuilder(String key, Object value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public TreeMap<String, Object> build() {
+        List<String> strings = splitKey(key);
+        TreeMap<String, Object> rootTree = new TreeMap<>();
+        TreeMap<String, Object> leafTree = strings.stream()
+                .reduce(new TreeMap<>(),
+                        (a, b) -> {
+                            TreeMap<String, Object> m = new TreeMap<>();
+                            if (!a.isEmpty()) {
+                                m.put(b, a);
+                            } else {
+                                m.put(b, value);
+                            }
+                            return m;
+                        },
+                        (a, b) -> {
+                            if (!b.isEmpty()) {
+                                a.put(b.firstKey(), b);
+                            }
+                            return a;
+                        });
+        createLeaf(rootTree, leafTree);
+        return rootTree;
+    }
+
+    private List<String> splitKey(String key) {
+        String[] split = key.split("\\.");
+        List<String> strings = Arrays.asList(split);
+        Collections.reverse(strings);
+        return strings;
+    }
+
+    private void createLeaf(TreeMap<String, Object> rootTree, TreeMap<String, Object> leafTree) {
+        for (Map.Entry<String, Object> reducedEntries : leafTree.entrySet()) {
+            Object result = rootTree.get(reducedEntries.getKey());
+            if (result == null) {
+                rootTree.put(reducedEntries.getKey(), reducedEntries.getValue());
+            } else {
+                Object o = rootTree.get(reducedEntries.getKey());
+                if (o instanceof TreeMap && result instanceof TreeMap) {
+                    createLeaf((TreeMap) o, (TreeMap) reducedEntries.getValue());
+                } else {
+                    throw new IllegalArgumentException("D");
+                }
+            }
+        }
+    }
+}
--- src/main/java/io/codearte/p2yaml/TreeBuilder.java
+++ src/main/java/io/codearte/p2yaml/TreeBuilder.java
@@ -1,9 +1,5 @@
 package io.codearte.p2yaml;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
 
@@ -17,54 +13,14 @@ public class TreeBuilder {
 
     public TreeMap<String, Object> build() {
         TreeMap<String, Object> rootTree = new TreeMap<>();
-        for (final String name : properties.stringPropertyNames()) {
-            breakProperty(rootTree, name);
-        }
+        properties.stringPropertyNames().forEach(
+                propertyName -> breakProperty(rootTree, propertyName)
+        );
         return rootTree;
     }
 
-    private void breakProperty(TreeMap<String, Object> rootTree, String key) {
-        String[] split = key.split("\\.");
-
-        List<String> strings = Arrays.asList(split);
-        Collections.reverse(strings);
-
-        final String value = properties.getProperty(key);
-
-        TreeMap<String, Object> leafTree = strings.stream()
-                .reduce(new TreeMap<>(),
-                        (a, b) -> {
-                            TreeMap<String, Object> m = new TreeMap<>();
-                            if (!a.isEmpty()) {
-                                m.put(b, a);
-                            } else {
-                                m.put(b, ValueConverter.asObject(value));
-                            }
-                            return m;
-                        },
-                        (a, b) -> {
-                            if (!b.isEmpty())
-                                a.put(b.firstKey(), b);
-                            return a;
-                        });
-        createLeaf(rootTree, leafTree);
-    }
-
-
-
-    private void createLeaf(TreeMap<String, Object> rootTree, TreeMap<String, Object> leafTree) {
-        for (Map.Entry<String, Object> reducedEntries : leafTree.entrySet()) {
-            Object result = rootTree.get(reducedEntries.getKey());
-            if (result == null) {
-                rootTree.put(reducedEntries.getKey(), reducedEntries.getValue());
-            } else {
-                Object o = rootTree.get(reducedEntries.getKey());
-                if (o instanceof TreeMap && result instanceof TreeMap) {
-                    createLeaf((TreeMap) o, (TreeMap) reducedEntries.getValue());
-                } else {
-                    throw new IllegalArgumentException("D");
-                }
-            }
-        }
+    private void breakProperty(TreeMap<String, Object> rootTree, String propertyName) {
+        final String value = properties.getProperty(propertyName);
+        rootTree.putAll(new KeyTreeBuilder(propertyName, ValueConverter.asObject(value)).build());
     }
 }
