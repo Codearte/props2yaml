0d01025938510ff0a67f49993320a762055ef82e
Mariusz Smykula <mariuszs@gmail.com>
2016-04-02 11:39:31 +0200

    cleanup test parameters

--- maven-plugin/src/test/java/io/codearte/props2yaml/maven/IntegrationTest.java
+++ maven-plugin/src/test/java/io/codearte/props2yaml/maven/IntegrationTest.java
@@ -1,5 +1,6 @@
 package io.codearte.props2yaml.maven;
 
+import io.takari.maven.testing.TestProperties;
 import io.takari.maven.testing.TestResources;
 import io.takari.maven.testing.executor.MavenRuntime;
 import io.takari.maven.testing.executor.MavenRuntime.MavenRuntimeBuilder;
@@ -12,7 +13,6 @@ import org.junit.runner.RunWith;
 import java.io.File;
 
 import static java.lang.String.format;
-import static java.lang.System.getProperty;
 
 @RunWith(MavenJUnitTestRunner.class)
 @MavenVersions({"3.3.3"})
@@ -21,8 +21,9 @@ public class IntegrationTest {
     @Rule
     public final TestResources resources = new TestResources();
 
-    private final MavenRuntime maven;
+    private final TestProperties properties = new TestProperties();
 
+    private final MavenRuntime maven;
 
     public IntegrationTest(MavenRuntimeBuilder builder) throws Exception {
         this.maven = builder.build();
@@ -31,7 +32,7 @@ public class IntegrationTest {
     @Test
     public void testBasic() throws Exception {
         File basedir = resources.getBasedir("basic");
-        String goal = format("io.codearte.props2yaml:props2yaml-maven-plugin:%s:convert", getProperty("projectVersion"));
+        String goal = format("io.codearte.props2yaml:props2yaml-maven-plugin:%s:convert", properties.getPluginVersion());
         maven.forProject(basedir)
                 .execute(goal, "-Dproperties=sample.properties")
                 .assertErrorFreeLog();
